b46fe3d38c3023a91bf71c63972b4a611c8bf7d2
Hossein Taleghani <hossein@arvancloud.com>
2020-11-20 21:36:06 +0330

    init with legacy code

---
+++ src/GameRunner.php
@@ -0,0 +1,22 @@
+<?php
+
+include __DIR__ . 'Game.php';
+
+$notAWinner = false;
+
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+do {
+    $aGame->roll(rand(0,5) + 1);
+
+    if (rand(0,9) == 7) {
+      $notAWinner = $aGame->wrongAnswer();
+    } else {
+      $notAWinner = $aGame->wasCorrectlyAnswered();
+    }
+} while ($notAWinner);
+  
